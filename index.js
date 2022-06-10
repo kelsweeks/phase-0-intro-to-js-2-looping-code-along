f714551867bb7dc74e320f37513be979c0239633
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-06-10 18:25:21 -0500

    commit tree a3ec505509a4004ee3d1231060ac5377676c5407

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+
+function writeCards() {
+    let thankYouMessages = ["Thank you, Guadalupe, for the wonderful surprise gift!", "Thank you, Ollie, for the wonderful surprise gift!", "Thank you, Aki, for the wonderful surprise gift!"]
+    return thankYouMessages
+}
+// invokes console.log once for each number
+// counting down from the number provided to zero
+const arrayOfNumbers = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,])
+
+function countDown() {
+    for (const element of arrayOfNumbers) {
+        console.log(element)
+    }
+}
+
